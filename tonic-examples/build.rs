66fc6eda0e894fb6f89052cdd0c79b85194a48c8
John Doneth <doneth7@gmail.com>
2019-09-20 21:33:55 -0400

    remove commented-out code

--- tonic-examples/build.rs
+++ tonic-examples/build.rs
@@ -1,17 +1,3 @@
 fn main() {
     tonic_build::compile_protos().unwrap();
-
-    /*tonic_build::compile_protos(
-        &["proto/helloworld/helloworld.proto"],
-        &["proto/helloworld"],
-        "helloworld",
-    )
-    .unwrap();
-
-    tonic_build::compile_protos(
-        &["proto/routeguide/route_guide.proto"],
-        &["proto/routeguide"],
-        "routeguide",
-    )
-    .unwrap();*/
 }
