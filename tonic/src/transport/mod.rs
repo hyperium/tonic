95bc8ee33a48ad451434275ac100ee415b0bcebd
James Nugent <james@jen20.com>
2019-10-07 13:53:17 +0200

    Rework TLS configuration to use a builder
    This commit reworks TLS configuration of both servers and endpoints in
    order to provide a more flexible API. We now add options to configure
    the selected TLS library using the appropriate 'native' configuration
    structures, as well as retaining the existing simplier interface which
    is compatible with both.
    
    The new API can also be easily extended to support simple interfaces for
    configuring mTLS and a range of other options without creating sprawl
    in the builders for `Server` and `Endpoint`.

--- tonic/src/transport/mod.rs
+++ tonic/src/transport/mod.rs
@@ -19,7 +19,7 @@
 //! ## Client
 //!
 //! ```no_run
-//! # use tonic::transport::{Channel, Certificate};
+//! # use tonic::transport::{Channel, Certificate, ClientTlsConfig};
 //! # use std::time::Duration;
 //! # use tonic::body::BoxBody;
 //! # use tonic::client::GrpcService;;
@@ -29,7 +29,9 @@
 //! let cert = std::fs::read_to_string("ca.pem")?;
 //!
 //! let mut channel = Channel::from_static("https://example.com")
-//!     .rustls_tls(Certificate::from_pem(&cert), "example.com".to_string())
+//!     .tls_config(ClientTlsConfig::with_rustls()
+//!         .ca_certificate(Certificate::from_pem(&cert))
+//!         .domain_name("example.com".to_string()))
 //!     .timeout(Duration::from_secs(5))
 //!     .rate_limit(5, Duration::from_secs(1))
 //!     .concurrency_limit(256)
@@ -43,7 +45,7 @@
 //! ## Server
 //!
 //! ```no_run
-//! # use tonic::transport::{Server, Identity};
+//! # use tonic::transport::{Server, Identity, ServerTlsConfig};
 //! # use tower::{Service, service_fn};
 //! # use futures_util::future::{err, ok};
 //! # #[cfg(feature = "rustls")]
@@ -55,7 +57,8 @@
 //! let addr = "[::1]:50051".parse()?;
 //!
 //! Server::builder()
-//!     .rustls_tls(Identity::from_pem(&cert, &key))
+//!     .tls_config(ServerTlsConfig::with_rustls()
+//!         .identity(Identity::from_pem(&cert, &key)))
 //!     .concurrency_limit_per_connection(256)
 //!     .interceptor_fn(|svc, req| {
 //!         println!("Request: {:?}", req);
@@ -88,5 +91,10 @@ pub use self::error::Error;
 pub use self::server::Server;
 pub use self::tls::{Certificate, Identity};
 pub use hyper::Body;
+
+#[cfg(feature = "tls")]
+pub use self::endpoint::ClientTlsConfig;
+#[cfg(feature = "tls")]
+pub use self::server::ServerTlsConfig;
 
 pub(crate) use self::error::ErrorKind;
