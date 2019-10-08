2af7040ab731ba55ec482b04a44181d4e141d44e
Adam Jacob <adam@stalecoffee.org>
2019-10-08 14:09:09 -0700

    feat(build): Expose type_attribute and field_attribute
    This commit exposes the `type_attribute` and `field_attribute`
    configuration settings from Prost. These are useful to tweak/extend the
    generated types.
    
    For example:
    
    ```
    tonic_build::configure()
        .out_dir(tmp)
        .format(false)
        .type_attribute(".", "#[derive(Serialize, Deserialize)]")
        .type_attribute(".", "#[serde(rename_all = \"camelCase\")]")
        .field_attribute("in", "#[serde(rename = \"in\")]")
        .compile(&["tests/protos/wellknown.proto"], &["tests/protos"])
        .unwrap();
    ```
    
    Would add the serde `Serialize` and `Deserialize` traits, while renaming
    all the fields to camelCase, and having serde keep fields named `in`
    named `in`, rather than Prost's `in_`, to every type generated by Prost.

--- tonic-build/tests/wellknown.rs
+++ tonic-build/tests/wellknown.rs
@@ -4,6 +4,9 @@ fn wellknown() {
     tonic_build::configure()
         .out_dir(tmp)
         .format(false)
+        .type_attribute(".", "#[derive(Serialize, Deserialize)]")
+        .type_attribute(".", "#[serde(rename_all = \"camelCase\")]")
+        .field_attribute("in", "#[serde(rename = \"in\")]")
         .compile(&["tests/protos/wellknown.proto"], &["tests/protos"])
         .unwrap();
 }
