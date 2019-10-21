bdcac3716aabc5d7398f93b50e96fc502028e99f
Phillip Cloud <cloud@standard.ai>
2019-10-21 19:38:48 -0400

    Modify existing test to send partial bodies

--- tonic/src/codec/tests.rs
+++ tonic/src/codec/tests.rs
@@ -30,30 +30,25 @@ async fn decode() {
     let mut buf = BytesMut::new();
     let len = msg.encoded_len();
 
-    // encode a few messages
-    let nmessages = 3;
-    buf.reserve(nmessages * (len + 5));
-
-    for _ in 0..nmessages {
-        buf.put_u8(0);
-        buf.put_u32_be(len as u32);
-        msg.encode(&mut buf).unwrap();
-    }
+    buf.reserve(len + 5);
+    buf.put_u8(0);
+    buf.put_u32_be(len as u32);
+    msg.encode(&mut buf).unwrap();
 
     let body = MockBody {
         data: buf.freeze(),
-        partial_len: 10010,
+        partial_len: 10005,
+        count: 0,
     };
 
     let mut stream = Streaming::new_request(decoder, body);
 
     let mut i = 0usize;
-    println!("");
     while let Some(msg) = stream.message().await.unwrap() {
         assert_eq!(msg.data.len(), data_len);
         i += 1;
     }
-    assert_eq!(i, nmessages);
+    assert_eq!(i, 1);
 }
 
 #[tokio::test]
@@ -78,7 +73,12 @@ async fn encode() {
 #[derive(Debug)]
 struct MockBody {
     data: Bytes,
+
+    // the size of the partial message to send
     partial_len: usize,
+
+    // the number of times we've sent
+    count: usize,
 }
 
 impl Body for MockBody {
@@ -87,15 +87,27 @@ impl Body for MockBody {
 
     fn poll_data(
         mut self: Pin<&mut Self>,
-        _cx: &mut Context<'_>,
+        cx: &mut Context<'_>,
     ) -> Poll<Option<Result<Self::Data, Self::Error>>> {
-        let partial_len = self.partial_len;
+        // every other call to poll_data returns data
+        let should_send = self.count % 2 == 0;
         let data_len = self.data.len();
-        let bytes_to_read = partial_len.min(data_len);
-        println!("");
-        if bytes_to_read > 0 {
-            let response = self.data.split_to(bytes_to_read).into_buf();
-            Poll::Ready(Some(Ok(Data(response))))
+        let partial_len = self.partial_len;
+        let count = self.count;
+        if data_len > 0 {
+            let result = if should_send {
+                let response = self
+                    .data
+                    .split_to(if count == 0 { partial_len } else { data_len })
+                    .into_buf();
+                Poll::Ready(Some(Ok(Data(response))))
+            } else {
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            };
+            // make some fake progress
+            self.count += 1;
+            result
         } else {
             Poll::Ready(None)
         }
