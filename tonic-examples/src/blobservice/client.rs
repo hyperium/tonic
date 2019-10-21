306bae4596583a9dce90aebdec336aedf9c0f7b6
Phillip Cloud <cloud@standard.ai>
2019-10-21 10:49:00 -0400

    Remove logging dependencies

--- tonic-examples/src/blobservice/client.rs
+++ tonic-examples/src/blobservice/client.rs
@@ -1,6 +1,4 @@
-use env_logger;
 use futures::TryStreamExt;
-use log::{debug, error};
 
 pub mod blobservice {
     tonic::include_proto!("blobservice");
@@ -10,7 +8,6 @@ use blobservice::{client::BlobberClient, BlobRequest};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
     let mut client = BlobberClient::connect("http://[::1]:50051")?;
     let nbytes = std::env::args().nth(1).unwrap().parse::<u64>().unwrap();
     let request = tonic::Request::new(BlobRequest { nbytes });
@@ -19,13 +16,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut inner = response.into_inner();
     let mut i = 0_u64;
     while let Some(_) = inner.try_next().await.map_err(|e| {
-        error!("i={}", i);
-        error!("message={}", e.message());
+        println!("i={}", i);
+        println!("message={}", e.message());
         e
     })? {
-        if i % 1000 == 0 {
-            debug!("request # {}", i);
-        }
         i += 1;
     }
     Ok(())
