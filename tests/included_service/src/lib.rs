b02b4b238bfee96b886609396b957e2592477ecb
James Steele <steele@users.noreply.github.com>
2019-11-09 15:45:22 +0000

    fix(build): Prevent duplicated client/server generated code (#121)
    * fix(build): Prevent duplicated client/server generated code
    
    The tonic-build process collects up RPC services as they are provided by
    prost, before writing them out as part of the finalization step for a
    given protocol buffer package.
    
    In the case of imported protocol buffer packages, there may be RPC
    services included by import in addition to those in the top-level
    package. Therefore it is necessary to make sure each set of
    client/server services gathered by tonic-build is cleared after the
    finalization process for a given protocol buffer package, otherwise they
    will be incorrectly aggregated as the generation process proceeds
    through the subsequent packages.
    
    * Test case for duplicated client/server generated code
    
    A simple test case that will fail to build without a fix to prevent
    RPC services being duplicated into inappropriate modules (that related
    to particular protocol buffer packages).
    
    * Additional test case for included_service
    
    Introduces an additional case that captures making sure services defined
    before including a package with additional services doesn't
    incidentially clear such precursor services from the including package.
    
    * Fix unnecessary newline to keep `cargo fmt` happy

---
+++ tests/included_service/src/lib.rs
@@ -0,0 +1,8 @@
+pub mod pb {
+    tonic::include_proto!("includer");
+}
+
+// Ensure that an RPC service, defined before including a file that defines
+// another service in a different protocol buffer package, is not incorrectly
+// cleared from the context of its package.
+type _Test = dyn pb::server::TopService;
