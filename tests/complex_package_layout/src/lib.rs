e52b60d15c67b53b7ccba6ac79dc87c6a54b2c53
Jay Oster <jay@pubnub.com>
2019-11-22 14:33:17 -0800

    Fix typo in comment

--- tests/complex_package_layout/src/lib.rs
+++ tests/complex_package_layout/src/lib.rs
@@ -2,7 +2,7 @@ mod pb {
     tonic::include_proto!("helloworld");
 }
 
-// Ensure that multiple services defined for a single pacakge, spread across multiple `.proto`
+// Ensure that multiple services defined for a single package, spread across multiple `.proto`
 // files, are all available in the generated Rust module.
 type _Test1 = dyn pb::server::Greeting;
 type _Test2 = dyn pb::server::Farewell;
