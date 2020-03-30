5cf166ed5741b38ef277d2706e9d40f2811cc0e6
Lucio Franco <luciofranco14@gmail.com>
2020-03-30 11:22:36 -0400

    more doc fixes
    Signed-off-by: Lucio Franco <luciofranco14@gmail.com>

--- tonic-build/src/lib.rs
+++ tonic-build/src/lib.rs
@@ -55,20 +55,25 @@
 #![doc(html_root_url = "https://docs.rs/tonic-build/0.1.0")]
 #![doc(issue_tracker_base_url = "https://github.com/hyperium/tonic/issues/")]
 #![doc(test(no_crate_inject, attr(deny(rust_2018_idioms))))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 use proc_macro2::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream};
 use quote::TokenStreamExt;
 
 /// Prost generator
 #[cfg(feature = "prost")]
+#[cfg_attr(docsrs, doc(cfg(feature = "prost")))]
 mod prost;
 
 #[cfg(feature = "prost")]
+#[cfg_attr(docsrs, doc(cfg(feature = "prost")))]
 pub use prost::{compile_protos, configure, Builder};
 
 #[cfg(feature = "rustfmt")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rustfmt")))]
 use std::io::{self, Write};
 #[cfg(feature = "rustfmt")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rustfmt")))]
 use std::process::{exit, Command};
 
 /// Service code generation for client
@@ -83,24 +88,24 @@ pub mod server;
 /// to allow any codegen module to generate service
 /// abstractions.
 pub trait Service {
-    /// Path to the codec
+    /// Path to the codec.
     const CODEC_PATH: &'static str;
 
-    /// Comment type
+    /// Comment type.
     type Comment: AsRef<str>;
 
-    /// Method type
+    /// Method type.
     type Method: Method;
 
-    /// Name of service
+    /// Name of service.
     fn name(&self) -> &str;
-    /// Package name of service
+    /// Package name of service.
     fn package(&self) -> &str;
-    /// Identifier used to generate type name
+    /// Identifier used to generate type name.
     fn identifier(&self) -> &str;
-    /// Methods provided by service
+    /// Methods provided by service.
     fn methods(&self) -> &[Self::Method];
-    /// Get comments about this item
+    /// Get comments about this item.
     fn comment(&self) -> &[Self::Comment];
 }
 
@@ -111,27 +116,28 @@ pub trait Service {
 /// to generate abstraction implementations for
 /// the provided methods.
 pub trait Method {
-    /// Path to the codec
+    /// Path to the codec.
     const CODEC_PATH: &'static str;
     /// Comment type.
     type Comment: AsRef<str>;
 
-    /// Name of method
+    /// Name of method.
     fn name(&self) -> &str;
-    /// Identifier used to generate type name
+    /// Identifier used to generate type name.
     fn identifier(&self) -> &str;
-    /// Method is streamed by client
+    /// Method is streamed by client.
     fn client_streaming(&self) -> bool;
-    /// Method is streamed by server
+    /// Method is streamed by server.
     fn server_streaming(&self) -> bool;
-    /// Get comments about this item
+    /// Get comments about this item.
     fn comment(&self) -> &[Self::Comment];
-    /// Type name of request and response
+    /// Type name of request and response.
     fn request_response_name(&self, proto_path: &str) -> (TokenStream, TokenStream);
 }
 
 /// Format files under the out_dir with rustfmt
 #[cfg(feature = "rustfmt")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rustfmt")))]
 pub fn fmt(out_dir: &str) {
     let dir = std::fs::read_dir(out_dir).unwrap();
 
