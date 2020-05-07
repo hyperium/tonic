8296b703add8bb70f38f8315a5b3ae9fce276b45
Lucio Franco <luciofranco14@gmail.com>
2020-05-07 11:33:41 -0400

    chore: Add integration tests
    Signed-off-by: Lucio Franco <luciofranco14@gmail.com>

---
+++ tests/integration_tests/tests/status.rs
@@ -0,0 +1,52 @@
+use bytes::Bytes;
+use futures_util::FutureExt;
+use integration_tests::pb::{test_client, test_server, Input, Output};
+use std::time::Duration;
+use tokio::sync::oneshot;
+use tonic::{transport::Server, Code, Request, Response, Status};
+
+#[tokio::test]
+async fn status_with_details() {
+    struct Svc;
+
+    #[tonic::async_trait]
+    impl test_server::Test for Svc {
+        async fn unary_call(&self, _: Request<Input>) -> Result<Response<Output>, Status> {
+            Err(Status::with_details(
+                Code::ResourceExhausted,
+                "Too many requests",
+                Bytes::from_static(b"hello"),
+            ))
+        }
+    }
+
+    let svc = test_server::TestServer::new(Svc);
+
+    let (tx, rx) = oneshot::channel::<()>();
+
+    let jh = tokio::spawn(async move {
+        Server::builder()
+            .add_service(svc)
+            .serve_with_shutdown("127.0.0.1:1337".parse().unwrap(), rx.map(drop))
+            .await
+            .unwrap();
+    });
+
+    tokio::time::delay_for(Duration::from_millis(100)).await;
+
+    let mut channel = test_client::TestClient::connect("http://127.0.0.1:1337")
+        .await
+        .unwrap();
+
+    let err = channel
+        .unary_call(Request::new(Input {}))
+        .await
+        .unwrap_err();
+
+    assert_eq!(err.message(), "Too many requests");
+    assert_eq!(err.details(), b"hello");
+
+    tx.send(()).unwrap();
+
+    jh.await.unwrap();
+}
