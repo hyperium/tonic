ff445fd2b456580b9817b949e9e5860667cae4c4
Juan Alvarez <j@yabit.io>
2020-09-22 13:54:50 -0500

    compute user agent in layer constructor

--- tonic/src/transport/service/user_agent.rs
+++ tonic/src/transport/service/user_agent.rs
@@ -7,11 +7,21 @@ const TONIC_USER_AGENT: &str = concat!("tonic/", env!("CARGO_PKG_VERSION"));
 #[derive(Debug)]
 pub(crate) struct UserAgent<T> {
     inner: T,
-    user_agent: Option<HeaderValue>,
+    user_agent: HeaderValue,
 }
 
 impl<T> UserAgent<T> {
     pub(crate) fn new(inner: T, user_agent: Option<HeaderValue>) -> Self {
+        let user_agent = user_agent
+            .map(|value| {
+                let mut buf = Vec::new();
+                buf.extend(value.as_bytes());
+                buf.push(b' ');
+                buf.extend(TONIC_USER_AGENT.as_bytes());
+                HeaderValue::from_bytes(&buf).expect("user-agent should be valid")
+            })
+            .unwrap_or(HeaderValue::from_static(TONIC_USER_AGENT));
+
         Self { inner, user_agent }
     }
 }
@@ -29,18 +39,8 @@ where
     }
 
     fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
-        let value = match self.user_agent.as_ref() {
-            Some(custom_ua) => {
-                let mut buf = Vec::new();
-                buf.extend(custom_ua.as_bytes());
-                buf.push(b' ');
-                buf.extend(TONIC_USER_AGENT.as_bytes());
-                HeaderValue::from_bytes(&buf).expect("user-agent should be valid")
-            }
-            None => HeaderValue::from_static(TONIC_USER_AGENT),
-        };
-
-        req.headers_mut().insert(USER_AGENT, value);
+        req.headers_mut()
+            .insert(USER_AGENT, self.user_agent.clone());
 
         self.inner.call(req)
     }
