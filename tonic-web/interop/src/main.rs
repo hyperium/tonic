e81c91999b18c1b0e3a74f4f2d33a03183345d73
Juan Alvarez <j@yabit.io>
2020-09-29 17:47:26 -0500

    rename crate, trim interop

---
+++ tonic-web/interop/src/main.rs
@@ -0,0 +1,17 @@
+use interop::server::{EchoHeadersSvc, TestService, TestServiceServer};
+use tonic::transport::Server;
+use tonic_grpc_web::GrpcWeb;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = ([127, 0, 0, 1], 9999).into();
+    let test_svc = TestServiceServer::new(TestService::default());
+    let with_echo = EchoHeadersSvc::new(test_svc);
+
+    Server::builder()
+        .add_service(GrpcWeb::new(with_echo))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
