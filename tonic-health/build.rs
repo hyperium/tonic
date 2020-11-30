3bda928cb0a6d3c86b745302f501704ada397cdf
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2020-11-30 14:14:59 -0500

    don't format generated code in tonic-health

--- tonic-health/build.rs
+++ tonic-health/build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
-        .format(true)
+        .format(false)
         .compile(&["proto/health.proto"], &["proto/"])?;
 
     Ok(())
