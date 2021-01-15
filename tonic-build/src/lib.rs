fda2d689d41625736e77a84b958b79517fb65772
Lucio Franco <luciofranco14@gmail.com>
2021-01-15 13:38:36 -0500

    chore: Prepare `0.4` release (#538)
    * Prepare `0.4` release
    
    * Fix cargo issues

--- tonic-build/src/lib.rs
+++ tonic-build/src/lib.rs
@@ -67,7 +67,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tokio-rs/website/master/public/img/icons/tonic.svg"
 )]
-#![doc(html_root_url = "https://docs.rs/tonic-build/0.3.1")]
+#![doc(html_root_url = "https://docs.rs/tonic-build/0.4.0")]
 #![doc(issue_tracker_base_url = "https://github.com/hyperium/tonic/issues/")]
 #![doc(test(no_crate_inject, attr(deny(rust_2018_idioms))))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
