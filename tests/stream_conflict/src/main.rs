6c898a239fd5e0000ec8120ef29a0444f3680f2e
Ben Sully <ben.sully88@gmail.com>
2021-03-28 18:31:20 +0100

    Use fully qualified path for futures_core::Stream in codegen (#591)

---
+++ tests/stream_conflict/src/main.rs
@@ -0,0 +1,7 @@
+mod stream_conflict {
+    tonic::include_proto!("stream_conflict");
+}
+
+fn main() {
+    println!("Hello, world!");
+}
--- tonic-build/src/server.rs
+++ tonic-build/src/server.rs
@@ -182,7 +182,7 @@ fn generate_trait_methods<T: Service>(
 
                 quote! {
                     #stream_doc
-                    type #stream: Stream<Item = Result<#res_message, tonic::Status>> + Send + Sync + 'static;
+                    type #stream: futures_core::Stream<Item = Result<#res_message, tonic::Status>> + Send + Sync + 'static;
 
                     #method_doc
                     async fn #name(&self, request: tonic::Request<#req_message>)
@@ -198,7 +198,7 @@ fn generate_trait_methods<T: Service>(
 
                 quote! {
                     #stream_doc
-                    type #stream: Stream<Item = Result<#res_message, tonic::Status>> + Send + Sync + 'static;
+                    type #stream: futures_core::Stream<Item = Result<#res_message, tonic::Status>> + Send + Sync + 'static;
 
                     #method_doc
                     async fn #name(&self, request: tonic::Request<tonic::Streaming<#req_message>>)
