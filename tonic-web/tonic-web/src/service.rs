e1993877c430906500aeda9ab1e3413e68ed483d
gmorer <gmorer@users.noreply.github.com>
2021-06-02 13:59:11 +0200

    fix(web): fix compilation (#670)
    Make tonic::body::empty_body public and use it instead of the old and non existant anymore BoxBody::empty()

--- tonic-web/tonic-web/src/service.rs
+++ tonic-web/tonic-web/src/service.rs
@@ -2,7 +2,7 @@ use std::task::{Context, Poll};
 
 use http::{header, HeaderMap, HeaderValue, Method, Request, Response, StatusCode, Version};
 use hyper::Body;
-use tonic::body::BoxBody;
+use tonic::body::{BoxBody, empty_body};
 use tonic::transport::NamedService;
 use tower_service::Service;
 use tracing::{debug, trace};
@@ -65,7 +65,7 @@ where
     fn no_content(&self, headers: HeaderMap) -> BoxFuture<S::Response, S::Error> {
         let mut res = Response::builder()
             .status(StatusCode::NO_CONTENT)
-            .body(BoxBody::empty())
+            .body(empty_body())
             .unwrap();
 
         res.headers_mut().extend(headers);
@@ -77,7 +77,7 @@ where
         Box::pin(async move {
             Ok(Response::builder()
                 .status(status)
-                .body(BoxBody::empty())
+                .body(empty_body())
                 .unwrap())
         })
     }
