50aa2e23977795b2e98aa18c66751fafa5621644
David Pedersen <david.pdrsn@gmail.com>
2021-06-28 12:00:58 +0200

    Test sending compressed request to server that doesn't support it

--- tests/compression/src/compressing_request.rs
+++ tests/compression/src/compressing_request.rs
@@ -1,6 +1,8 @@
 use super::*;
 use http_body::Body as _;
 
+// TODO(david): send_gzip on channel, but disabling compression of a message
+
 #[tokio::test(flavor = "multi_thread")]
 async fn client_enabled_server_enabled() {
     let svc = test_server::TestServer::new(Svc).accept_gzip();
@@ -32,13 +34,18 @@ async fn client_enabled_server_enabled() {
         })
     };
 
+    fn assert_right_encoding<B>(req: http::Request<B>) -> http::Request<B> {
+        assert_eq!(req.headers().get("grpc-encoding").unwrap(), "gzip");
+        req
+    }
+
     tokio::spawn(async move {
         Server::builder()
             .layer(
                 ServiceBuilder::new()
-                    // TODO(david): require request to have `grpc-encoding: gzip`
                     .layer(
                         ServiceBuilder::new()
+                            .map_request(assert_right_encoding)
                             .layer(measure_request_body_size_layer)
                             .into_inner(),
                     )
@@ -69,4 +76,35 @@ async fn client_enabled_server_enabled() {
     assert!(bytes_sent < UNCOMPRESSED_MIN_BODY_SIZE);
 }
 
-// TODO(david): send_gzip on channel, but disabling compression of a message
+#[tokio::test(flavor = "multi_thread")]
+async fn client_enabled_server_disabled() {
+    let svc = test_server::TestServer::new(Svc);
+
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    tokio::spawn(async move {
+        Server::builder()
+            .add_service(svc)
+            .serve_with_incoming(tokio_stream::wrappers::TcpListenerStream::new(listener))
+            .await
+            .unwrap();
+    });
+
+    let channel = Channel::builder(format!("http://{}", addr).parse().unwrap())
+        .connect()
+        .await
+        .unwrap();
+
+    let mut client = test_client::TestClient::new(channel).send_gzip();
+
+    let status = client
+        .compress_input(SomeData {
+            data: [0_u8; UNCOMPRESSED_MIN_BODY_SIZE].to_vec(),
+        })
+        .await
+        .unwrap_err();
+
+    assert_eq!(status.code(), tonic::Code::Unimplemented);
+    assert_eq!(status.message(), "Request is compressed with `gzip` which the server doesn't support");
+}
