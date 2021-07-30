a94dff8c980ea278f04047ea285504a053a0e8dd
Tom Dyas <tdyas@toolchain.com>
2021-07-30 13:40:41 -0400

    failing test

--- tests/wellknown-compiled/build.rs
+++ tests/wellknown-compiled/build.rs
@@ -1,6 +1,13 @@
 fn main() {
+    let mut config = prost_build::Config::new();
+    config.extern_path(".google.protobuf.Empty", "()");
+
     tonic_build::configure()
         .compile_well_known_types(true)
-        .compile(&["proto/google.proto"], &["proto"])
+        .compile_with_config(
+            config,
+            &["proto/google.proto", "proto/test.proto"],
+            &["proto"],
+        )
         .unwrap();
 }
