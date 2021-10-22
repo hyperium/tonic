ff6a690cec9daca33984cabea66f9d370ac63462
Tom Dyas <tdyas@toolchain.com>
2021-10-22 08:44:27 -0400

    fix(build): Correctly convert `Empty` to `()` (#734)

--- tests/wellknown-compiled/src/lib.rs
+++ tests/wellknown-compiled/src/lib.rs
@@ -1,9 +1,18 @@
-pub mod google {
-    pub mod protobuf {
-        tonic::include_proto!("google.protobuf");
+pub mod gen {
+    pub mod google {
+        pub mod protobuf {
+            tonic::include_proto!("google.protobuf");
+        }
+    }
+
+    pub mod test {
+        tonic::include_proto!("test");
     }
 }
 
 pub fn grok() {
-    let _empty = crate::google::protobuf::Empty {};
+    let _any = crate::gen::google::protobuf::Any {
+        type_url: "foo".to_owned(),
+        value: Vec::new(),
+    };
 }
