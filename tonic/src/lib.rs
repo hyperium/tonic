e78a2d832fdab61e82586cc1cf954f31b16da02a
David Pedersen <david.pdrsn@gmail.com>
2021-12-08 17:42:16 +0100

    chore: Prepare 0.6.2 (#860)
    - Contains the revert to [rust 2018](https://github.com/hyperium/tonic/pull/847).
    - Bumps both tonic and tonic-build.
    - tonic-build goes from 0.6.0 to 0.6.2 since there was no 0.6.1. I
      believe we want tonic and tonic-build versions to follow each other.

--- tonic/src/lib.rs
+++ tonic/src/lib.rs
@@ -79,7 +79,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tokio-rs/website/master/public/img/icons/tonic.svg"
 )]
-#![doc(html_root_url = "https://docs.rs/tonic/0.6.1")]
+#![doc(html_root_url = "https://docs.rs/tonic/0.6.2")]
 #![doc(issue_tracker_base_url = "https://github.com/hyperium/tonic/issues/")]
 #![doc(test(no_crate_inject, attr(deny(rust_2018_idioms))))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
