ec738e2d64a43707848c72177cb3173b994248ae
super-horst <super-horst@posteo.net>
2022-01-09 01:45:33 +0100

    streaming client receives no echo

--- examples/src/streaming/client.rs
+++ examples/src/streaming/client.rs
@@ -8,16 +8,29 @@ use pb::{echo_client::EchoClient, EchoRequest};
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = EchoClient::connect("http://[::1]:50051").await.unwrap();
 
-    let stream = client
+    let mut stream = client
         .server_streaming_echo(EchoRequest {
             message: "foo".into(),
         })
         .await
-        .unwrap();
+        .unwrap().into_inner();
 
     println!("Connected...now sleeping for 2 seconds...");
 
     tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+
+    let incoming_message = stream.message().await;
+    match incoming_message {
+        Ok(Some(echo)) => {
+            println!("It echoed: {}", echo.message);
+        }
+        Ok(None) => {
+            println!("No message passed");
+        }
+        Err(status) => {
+            println!("Received status {}", status);
+        }
+    }
 
     // Disconnect
     drop(stream);
