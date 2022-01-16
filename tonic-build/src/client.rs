32f0ae83d7be37dfd55c18c05072ed93231f3819
cssivision <cssivision@gmail.com>
2022-01-16 12:00:36 +0800

    fix(tonic-build): fix clippy warns when a method returning Self doesn’t have the #[must_use] attribute

--- tonic-build/src/client.rs
+++ tonic-build/src/client.rs
@@ -82,12 +82,14 @@ pub fn generate<T: Service>(
                 ///
                 /// This requires the server to support it otherwise it might respond with an
                 /// error.
+                #[must_use]
                 pub fn send_gzip(mut self) -> Self {
                     self.inner = self.inner.send_gzip();
                     self
                 }
 
                 /// Enable decompressing responses with `gzip`.
+                #[must_use]
                 pub fn accept_gzip(mut self) -> Self {
                     self.inner = self.inner.accept_gzip();
                     self
