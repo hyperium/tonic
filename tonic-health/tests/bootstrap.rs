cb2eedeca2b43c2e04aa9e5d713adf505cb20658
Ben Sully <ben.sully@grafana.com>
2022-08-22 22:45:03 +0100

    Use bootstrap test to generate/check validity of generated code
    As suggested by @LucioFranco in https://github.com/hyperium/tonic/pull/1065\#discussion_r951580189.
    This avoids the need for a feature which would show
    up in docs.rs, and achieves the same goals via CI.

---
+++ tonic-health/tests/bootstrap.rs
@@ -0,0 +1,30 @@
+use std::{path::PathBuf, process::Command};
+
+#[test]
+fn bootstrap() {
+    let iface_files = &["proto/health.proto"];
+    let dirs = &["proto"];
+
+    let out_dir = PathBuf::from(std::env!("CARGO_MANIFEST_DIR"))
+        .join("src")
+        .join("generated");
+
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .out_dir(format!("{}", out_dir.display()))
+        .compile(iface_files, dirs)
+        .unwrap();
+
+    let status = Command::new("git")
+        .arg("diff")
+        .arg("--exit-code")
+        .arg("--")
+        .arg(format!("{}", out_dir.display()))
+        .status()
+        .unwrap();
+
+    if !status.success() {
+        panic!("You should commit the protobuf files");
+    }
+}
