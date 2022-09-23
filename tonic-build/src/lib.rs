201ed7a1f36c71f824b9aac7b37a17ca1fa56ba2
Marek Kuskowski <50183564+nylonicious@users.noreply.github.com>
2022-09-24 00:23:00 +0200

    feat(build): add newline for readability

--- tonic-build/src/lib.rs
+++ tonic-build/src/lib.rs
@@ -2,6 +2,7 @@
 //! and proto definitiones for use with `tonic`.
 //!
 //! # Feature flags
+//!
 //! - `cleanup-markdown`: Enables cleaning up documentation from the generated code. Useful
 //! when documentation of the generated code fails `cargo test --doc` for example.
 //! - `prost`: Enables usage of prost generator (enabled by default).
