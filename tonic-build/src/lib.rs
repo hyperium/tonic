21291bb68ea04486f7ffa51f5eae93cec63c4037
Marek Kuskowski <50183564+nylonicious@users.noreply.github.com>
2022-09-24 00:20:11 +0200

    feat(build): improve feature flag documentation

--- tonic-build/src/lib.rs
+++ tonic-build/src/lib.rs
@@ -3,7 +3,7 @@
 //!
 //! # Feature flags
 //! - `cleanup-markdown`: Enables cleaning up documentation from the generated code. Useful
-//! when documentation of generated code fails `cargo test --doc`.
+//! when documentation of the generated code fails `cargo test --doc` for example.
 //! - `prost`: Enables usage of prost generator (enabled by default).
 //! - `transport`: Enables generation of `connect` method using `tonic::transport::Channel`
 //! (enabled by default).
