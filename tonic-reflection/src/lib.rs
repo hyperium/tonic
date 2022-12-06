a78762bbd3d930494acaef4f9c750ca091f7091f
tottoto <tottotodev@gmail.com>
2022-12-06 22:06:13 +0900

    chore(reflection): Fix clippy lint

--- tonic-reflection/src/lib.rs
+++ tonic-reflection/src/lib.rs
@@ -19,6 +19,7 @@
 pub mod proto {
     #![allow(unreachable_pub)]
     #![allow(missing_docs)]
+    #![allow(clippy::derive_partial_eq_without_eq)]
     include!("generated/grpc.reflection.v1alpha.rs");
 
     pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("generated/reflection_v1alpha1.bin");
