b736cd1a7069c3196025ed2ec763d1a9d6abc456
tottoto <tottotodev@gmail.com>
2022-12-14 21:45:53 +0900

    chore(examples): Move attrs to separate crate

--- examples/attr/build.rs
+++ examples/attr/build.rs
@@ -1,17 +1,9 @@
 fn main() {
     tonic_build::configure()
-        .compile(&["proto/helloworld/helloworld.proto"], &["proto"])
-        .unwrap();
-
-    tonic_build::compile_protos("proto/echo/echo.proto").unwrap();
-
-    tonic_build::compile_protos("proto/unaryecho/echo.proto").unwrap();
-
-    tonic_build::configure()
         .server_mod_attribute("attrs", "#[cfg(feature = \"server\")]")
         .server_attribute("Echo", "#[derive(PartialEq)]")
         .client_mod_attribute("attrs", "#[cfg(feature = \"client\")]")
         .client_attribute("Echo", "#[derive(PartialEq)]")
-        .compile(&["proto/attrs/attrs.proto"], &["proto"])
+        .compile(&["proto/attrs.proto"], &["proto"])
         .unwrap();
 }
