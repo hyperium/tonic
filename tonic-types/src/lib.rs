3ee1e6cc1022abfcf9eeb8c71321a3b9c188ed9f
tottoto <tottotodev@gmail.com>
2023-01-05 04:19:47 +0900

    chore: Add test to check FILE_DESCRIPTOR_SET is valid (#1217)

--- tonic-types/src/lib.rs
+++ tonic-types/src/lib.rs
@@ -27,6 +27,17 @@ pub mod pb {
 
     /// Byte encoded FILE_DESCRIPTOR_SET.
     pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("generated/types.bin");
+
+    #[cfg(test)]
+    mod tests {
+        use super::FILE_DESCRIPTOR_SET;
+        use prost::Message as _;
+
+        #[test]
+        fn file_descriptor_set_is_valid() {
+            prost_types::FileDescriptorSet::decode(FILE_DESCRIPTOR_SET).unwrap();
+        }
+    }
 }
 
 pub use pb::Status;
