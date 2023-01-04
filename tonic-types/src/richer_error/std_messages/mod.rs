03b4735bb4ba7c6e84842d0515d1fd3be9d1cc13
Rafael Lemos <flemos.rafael.dev@gmail.com>
2023-01-04 16:24:09 -0300

    feat(types): Add gRPC Richer Error Model support (QuotaFailure) (#1204)

--- tonic-types/src/richer_error/std_messages/mod.rs
+++ tonic-types/src/richer_error/std_messages/mod.rs
@@ -6,6 +6,10 @@ mod debug_info;
 
 pub use debug_info::DebugInfo;
 
+mod quota_failure;
+
+pub use quota_failure::{QuotaFailure, QuotaViolation};
+
 mod bad_request;
 
 pub use bad_request::{BadRequest, FieldViolation};
