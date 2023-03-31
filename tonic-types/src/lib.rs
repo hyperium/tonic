0f3d77ca455a3f9568c94466d62ba7cb00cf4181
Lucio Franco <luciofranco14@gmail.com>
2023-03-31 14:25:13 -0400

    chore: Prepare `v0.9.0` release

--- tonic-types/src/lib.rs
+++ tonic-types/src/lib.rs
@@ -150,7 +150,7 @@
     html_logo_url = "https://raw.githubusercontent.com/tokio-rs/website/master/public/img/icons/tonic.svg"
 )]
 #![deny(rustdoc::broken_intra_doc_links)]
-#![doc(html_root_url = "https://docs.rs/tonic-types/0.6.1")]
+#![doc(html_root_url = "https://docs.rs/tonic-types/0.9.0")]
 #![doc(issue_tracker_base_url = "https://github.com/hyperium/tonic/issues/")]
 
 /// Useful protobuf types
