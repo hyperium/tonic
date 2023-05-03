9aa67be17e9a8a49a278cf5e12f69bff66643a24
Michalis Ioak <ioakimidismichalis@gmail.com>
2023-05-04 01:02:19 +0300

    added example (multiple protos)
    the protos are split between two files

---
+++ examples/src/multiple_protos/server.rs
@@ -0,0 +1,46 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use file2::{HelloReply, HelloRequest};
+use something_different::greeter_server::{Greeter, GreeterServer};
+
+//NOTE pub mod <package name> (in .proto file)
+pub mod something_different {
+    tonic::include_proto!("something_different");
+}
+
+pub mod file2 {
+    tonic::include_proto!("file2");
+}
+
+#[derive(Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse().unwrap();
+    let greeter = MyGreeter::default();
+
+    println!("GreeterServer listening on {}", addr);
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
