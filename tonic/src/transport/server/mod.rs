39d14bd4baee7f3e50da0672cb70bbd39c89510c
Arjun Patel <patel.arjun50@gmail.com>
2023-06-22 06:14:02 -0700

    fixing broken links for window size and max concurrent streams (#1411)

--- tonic/src/transport/server/mod.rs
+++ tonic/src/transport/server/mod.rs
@@ -194,7 +194,7 @@ impl<L> Server<L> {
     ///
     /// Default is 65,535
     ///
-    /// [spec]: https://http2.github.io/http2-spec/#SETTINGS_INITIAL_WINDOW_SIZE
+    /// [spec]: https://httpwg.org/specs/rfc9113.html#InitialWindowSize
     #[must_use]
     pub fn initial_stream_window_size(self, sz: impl Into<Option<u32>>) -> Self {
         Server {
@@ -219,7 +219,7 @@ impl<L> Server<L> {
     ///
     /// Default is no limit (`None`).
     ///
-    /// [spec]: https://http2.github.io/http2-spec/#SETTINGS_MAX_CONCURRENT_STREAMS
+    /// [spec]: https://httpwg.org/specs/rfc9113.html#n-stream-concurrency
     #[must_use]
     pub fn max_concurrent_streams(self, max: impl Into<Option<u32>>) -> Self {
         Server {
