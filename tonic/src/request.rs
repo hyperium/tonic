0c06ff26ba6c623019b74f61c43ffe2a1ebb2bd5
tottoto <tottotodev@gmail.com>
2023-06-24 08:25:20 +0900

    chore: Update document

--- tonic/src/request.rs
+++ tonic/src/request.rs
@@ -77,12 +77,11 @@ pub trait IntoRequest<T>: sealed::Sealed {
 /// # }
 /// # let client = Client {};
 /// use tonic::Request;
-/// use futures_util::stream;
 ///
 /// let messages = vec![Point {}, Point {}];
 ///
-/// client.record_route(Request::new(stream::iter(messages.clone())));
-/// client.record_route(stream::iter(messages));
+/// client.record_route(Request::new(tokio_stream::iter(messages.clone())));
+/// client.record_route(tokio_stream::iter(messages));
 /// ```
 pub trait IntoStreamingRequest: sealed::Sealed {
     /// The RPC request stream type
