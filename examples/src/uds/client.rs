144f105a77ba1168853c4c43ee142a4aa6beed98
Kristopher Wuollett <kris@appbiotic.com>
2024-01-30 07:53:53 -0600

    examples: allow configurable uds socket paths
    Update unix domain socket examples to show configurable paths. It
    demonstrates a way of configuring the UDS client using the ustr crate to
    meet the closure capture requirements of the connector service_fn.
    
    Fixes: #1611
    Refs: #1612

--- examples/src/uds/client.rs
+++ examples/src/uds/client.rs
@@ -5,21 +5,28 @@ pub mod hello_world {
 }
 
 use hello_world::{greeter_client::GreeterClient, HelloRequest};
+use std::env;
 #[cfg(unix)]
 use tokio::net::UnixStream;
 use tonic::transport::{Endpoint, Uri};
 use tower::service_fn;
+use ustr::ustr;
 
 #[cfg(unix)]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Path needs to be a static str due to service_fn FnMut.
+    let path = match env::var("EXAMPLE_UDS_PATH") {
+        Ok(path) => ustr(path.as_ref()).as_str(),
+        Err(_) => "/tmp/tonic/helloworld"
+    };
+    println!("Using path {}", path);
+
     // We will ignore this uri because uds do not use it
     // if your connector does use the uri it will be provided
     // as the request to the `MakeConnection`.
     let channel = Endpoint::try_from("http://[::]:50051")?
-        .connect_with_connector(service_fn(|_: Uri| {
-            let path = "/tmp/tonic/helloworld";
-
+        .connect_with_connector(service_fn(move |_: Uri| {
             // Connect to a Uds socket
             UnixStream::connect(path)
         }))
