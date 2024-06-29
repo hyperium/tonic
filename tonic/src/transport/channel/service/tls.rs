917afee741840ee490723f970ece86a2593f663e
tottoto <tottotodev@gmail.com>
2024-06-29 19:24:09 +0900

    chore(tls): Refactor parsing Certificate

--- tonic/src/transport/channel/service/tls.rs
+++ tonic/src/transport/channel/service/tls.rs
@@ -1,5 +1,4 @@
 use std::fmt;
-use std::io::Cursor;
 use std::sync::Arc;
 
 use hyper_util::rt::TokioIo;
@@ -13,7 +12,7 @@ use tokio_rustls::{
 };
 
 use super::io::BoxedIo;
-use crate::transport::service::tls::{add_certs_from_pem, load_identity, TlsError, ALPN_H2};
+use crate::transport::service::tls::{load_identity, TlsError, ALPN_H2};
 use crate::transport::tls::{Certificate, Identity};
 
 #[derive(Clone)]
@@ -55,7 +54,7 @@ impl TlsConnector {
         }
 
         for cert in ca_certs {
-            add_certs_from_pem(&mut Cursor::new(cert), &mut roots)?;
+            roots.add_parsable_certificates(cert.parse()?);
         }
 
         let builder = builder.with_root_certificates(roots);
