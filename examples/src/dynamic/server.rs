315bb09a6ea5084828d60df014ed6b6311ef379c
tottoto <tottotodev@gmail.com>
2024-07-08 18:12:33 +0900

    feat(server): Remove reexporting service router (#1773)

--- examples/src/dynamic/server.rs
+++ examples/src/dynamic/server.rs
@@ -1,5 +1,5 @@
 use std::env;
-use tonic::{transport::server::RoutesBuilder, transport::Server, Request, Response, Status};
+use tonic::{service::RoutesBuilder, transport::Server, Request, Response, Status};
 
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
