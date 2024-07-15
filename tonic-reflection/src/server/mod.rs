4e1f94da0b3bb3ef21801cccd2141cd53148a79e
Jesper Svennevid <152262941+ttkjesper@users.noreply.github.com>
2024-07-15 14:16:43 +0200

    tonic-reflection: Restructure crate to prep reintroducing v1alpha
    To simplify API verification, this is a prepatory change to shift
    around the v1 support to allow for a cleaner v1alpha reintroduction.
    
    * Created a multi-file version of the server.rs module
    * Added mod.rs containing the base error with associated imports
    * Added v1.rs containing the v1 version of the reflection server

---
+++ tonic-reflection/src/server/mod.rs
@@ -0,0 +1,36 @@
+pub use crate::pb::v1::server_reflection_server::{ServerReflection, ServerReflectionServer};
+
+use prost::DecodeError;
+use std::fmt::{Display, Formatter};
+
+mod v1;
+
+pub use v1::Builder;
+
+/// Represents an error in the construction of a gRPC Reflection Service.
+#[derive(Debug)]
+pub enum Error {
+    /// An error was encountered decoding a `prost_types::FileDescriptorSet` from a buffer.
+    DecodeError(prost::DecodeError),
+    /// An invalid `prost_types::FileDescriptorProto` was encountered.
+    InvalidFileDescriptorSet(String),
+}
+
+impl From<DecodeError> for Error {
+    fn from(e: DecodeError) -> Self {
+        Error::DecodeError(e)
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::DecodeError(_) => f.write_str("error decoding FileDescriptorSet from buffer"),
+            Error::InvalidFileDescriptorSet(s) => {
+                write!(f, "invalid FileDescriptorSet - {}", s)
+            }
+        }
+    }
+}
