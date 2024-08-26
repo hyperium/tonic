54102ff356ea529dee1af8607ecab5354f96c354
tottoto <tottotodev@gmail.com>
2024-08-26 21:06:33 +0900

    chore(build): Update deprecated attributes test
    Co-authored-by: bytedream <63594396+bytedream@users.noreply.github.com>

--- tests/deprecated_methods/tests/deprecated_methods.rs
+++ tests/deprecated_methods/tests/deprecated_methods.rs
@@ -3,6 +3,11 @@ use std::{fs, path::PathBuf};
 #[test]
 fn test() {
     let path = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("test.rs");
-    let s = fs::read_to_string(path).unwrap();
-    assert_eq!(s.match_indices("#[deprecated]").count(), 1);
+    let s = fs::read_to_string(path)
+        .unwrap()
+        .split_whitespace()
+        .collect::<Vec<_>>()
+        .join(" ");
+    assert!(s.contains("#[deprecated] pub async fn deprecated("));
+    assert!(!s.contains("#[deprecated] pub async fn not_deprecated("));
 }
