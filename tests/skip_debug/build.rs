32c63757294fb16299e4db47791445b565ccb53d
tottoto <tottotodev@gmail.com>
2024-09-05 03:53:06 +0900

    chore(test): Refactor debug implementation for output

--- tests/skip_debug/build.rs
+++ tests/skip_debug/build.rs
@@ -7,20 +7,4 @@ fn main() {
         .build_server(true)
         .compile_protos_with_config(config, &["proto/test.proto"], &["proto"])
         .unwrap();
-
-    // Add a dummy impl Debug to the skipped debug implementations to avoid missing impl Debug errors
-    let out = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    let file_path = out.join("test.rs");
-    let mut file_contents = std::fs::read_to_string(&file_path).unwrap();
-    let debug_impl = r#"
-impl std::fmt::Debug for Output {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Output").finish()
-    }
-}
-"#;
-    file_contents.push_str(debug_impl);
-
-    // Replace the original file with the modified content
-    std::fs::write(&file_path, file_contents).unwrap();
 }
