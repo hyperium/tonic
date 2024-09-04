6ba10dfa239130c45c8352f1c66dbe2b9f92673c
tottoto <tottotodev@gmail.com>
2024-09-05 04:41:44 +0900

    chore(doc): Replace deprecated compile with compile_protos

--- tonic-build/src/lib.rs
+++ tonic-build/src/lib.rs
@@ -36,7 +36,7 @@
 //! fn main() -> Result<(), Box<dyn std::error::Error>> {
 //!    tonic_build::configure()
 //!         .build_server(false)
-//!         .compile(
+//!         .compile_protos(
 //!             &["proto/helloworld/helloworld.proto"],
 //!             &["proto/helloworld"],
 //!         )?;
