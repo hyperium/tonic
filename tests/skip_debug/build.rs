f056d8574fcf7c1c0afccc5d22867c1e3cda4a17
tottoto <tottotodev@gmail.com>
2024-09-05 03:54:52 +0900

    chore(test): Use compile_protos instead of providing default config

--- tests/skip_debug/build.rs
+++ tests/skip_debug/build.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let config = prost_build::Config::default();
     tonic_build::configure()
         .skip_debug("test.Test")
         .skip_debug("test.Output")
         .build_client(true)
         .build_server(true)
-        .compile_protos_with_config(config, &["proto/test.proto"], &["proto"])
+        .compile_protos(&["proto/test.proto"], &["proto"])
         .unwrap();
 }
