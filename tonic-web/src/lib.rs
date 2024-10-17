274cde8fee7c51abe0be0d62623ad09a8dcb24d3
tottoto <tottotodev@gmail.com>
2024-10-18 06:40:31 +0900

    chore: Start development of version 0.13

--- tonic-web/src/lib.rs
+++ tonic-web/src/lib.rs
@@ -94,7 +94,7 @@
     rust_2018_idioms,
     unreachable_pub
 )]
-#![doc(html_root_url = "https://docs.rs/tonic-web/0.12.3")]
+#![doc(html_root_url = "https://docs.rs/tonic-web/0.13.0")]
 #![doc(issue_tracker_base_url = "https://github.com/hyperium/tonic/issues/")]
 
 pub use call::GrpcWebCall;
