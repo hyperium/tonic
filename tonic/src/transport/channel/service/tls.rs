8def0bbcd00af9e1c2c8428630e34971e0f228f3
tottoto <tottotodev@gmail.com>
2024-11-06 01:17:14 +0900

    chore(tls): Refactor parsing certificate and identity (#1763)
    * chore(tls): Refactor parsing Certificate
    
    * chore(tls): Refactor parsing Identity
    
    * chore(tls): Change method to convert certificate and identity to rustls-pki-types type to independent function

--- tonic/src/transport/channel/service/tls.rs
+++ tonic/src/transport/channel/service/tls.rs
@@ -1,5 +1,4 @@
 use std::fmt;
-use std::io::Cursor;
 use std::sync::Arc;
 
 use hyper_util::rt::TokioIo;
@@ -13,7 +12,9 @@ use tokio_rustls::{
 };
 
 use super::io::BoxedIo;
-use crate::transport::service::tls::{add_certs_from_pem, load_identity, TlsError, ALPN_H2};
+use crate::transport::service::tls::{
+    convert_certificate_to_pki_types, convert_identity_to_pki_types, TlsError, ALPN_H2,
+};
 use crate::transport::tls::{Certificate, Identity};
 
 #[derive(Clone)]
@@ -55,13 +56,13 @@ impl TlsConnector {
         }
 
         for cert in ca_certs {
-            add_certs_from_pem(&mut Cursor::new(cert), &mut roots)?;
+            roots.add_parsable_certificates(convert_certificate_to_pki_types(&cert)?);
         }
 
         let builder = builder.with_root_certificates(roots);
         let mut config = match identity {
             Some(identity) => {
-                let (client_cert, client_key) = load_identity(identity)?;
+                let (client_cert, client_key) = convert_identity_to_pki_types(&identity)?;
                 builder.with_client_auth_cert(client_cert, client_key)?
             }
             None => builder.with_no_client_auth(),
