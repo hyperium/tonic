62f3022632ae5f829e5102ea88bcd1976fcfa134
tottoto <tottotodev@gmail.com>
2025-06-19 07:19:30 +0900

    chore(test): Simplify skip debug test

--- tests/skip_debug/tests/skip_debug.rs
+++
@@ -1,8 +0,0 @@
-use std::{fs, path::PathBuf};
-
-#[test]
-fn skip_debug() {
-    let path = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("test.rs");
-    let s = fs::read_to_string(path).unwrap();
-    assert!(s.contains("#[prost(skip_debug)]\npub struct Output {}"));
-}
