a7ba8383ce7977b10e47f044270c71940ab82775
Lucio Franco <luciofranco14@gmail.com>
2025-07-22 14:52:20 -0400

    chore: clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -11,23 +11,35 @@
 
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
-      inputs = { nixpkgs.follows = "nixpkgs"; };
-    };
-
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
     };
   };
 
-  outputs = inputs@{ self, flake-parts, ... }:
+  outputs =
+    inputs@{ self, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.git-hooks.flakeModule inputs.treefmt-nix.flakeModule ];
-      systems =
-        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, pkgs, system, ... }:
-        let rustToolchain = pkgs.fenix.stable;
-        in {
+      imports = [
+        inputs.git-hooks.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          rustToolchain = pkgs.fenix.stable;
+        in
+        {
           _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
             overlays = [ inputs.fenix.overlays.default ];
@@ -42,21 +54,35 @@
             settings.hooks = {
               actionlint.enable = true;
               shellcheck.enable = true;
-              treefmt.enable = true;
+              clippy = {
+                enable = true;
+                packageOverrides = {
+                  cargo = rustToolchain.cargo;
+                  clippy = rustToolchain.clippy;
+                };
+              };
+              cargo-check = {
+                enable = true;
+                package = rustToolchain.cargo;
+                entry = "${rustToolchain.cargo}/bin/cargo check --workspace --all-features";
+                files = "\\.rs$";
+                pass_filenames = false;
+              };
+              rustfmt = {
+                enable = true;
+                packageOverrides = {
+                  rustfmt = rustToolchain.rustfmt;
+                  cargo = rustToolchain.cargo;
+                };
+              };
             };
-          };
-
-          treefmt = {
-            settings = { rustfmt.enable = true; };
-            projectRootFile = ".git/config";
-            flakeCheck = false; # Covered by git-hooks check
           };
 
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
-              nixd
-              nixfmt
+              cargo-nextest
               bazel_7
+              pre-commit
 
               (rustToolchain.withComponents [
                 "cargo"
@@ -70,9 +96,9 @@
             ];
 
             shellHook = ''
-              # export PATH="$(pwd)/bazel-bin/src/:$PATH"
-              export PLUGIN_PATH="protoc-gen-rust-grpc/bazel-bin/src/protoc-gen-rust-grpc"
-              export PATH="protoc-gen-rust-grpc/bazel-bin/src:$PATH"
+              export PATH="$PWD/protoc-gen-rust-grpc/bazel-bin/src:$PATH"
+
+              ${config.pre-commit.installationScript}
             '';
           };
         };
