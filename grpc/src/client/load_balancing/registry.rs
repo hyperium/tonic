6aefef80084e716306a87380e4a86037d3fc5d20
tottoto <tottotodev@gmail.com>
2025-07-29 05:36:34 +0900

    chore(grpc): Remove once_cell dependency

--- grpc/src/client/load_balancing/registry.rs
+++ grpc/src/client/load_balancing/registry.rs
@@ -1,9 +1,7 @@
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex},
+    sync::{Arc, LazyLock, Mutex},
 };
-
-use once_cell::sync::Lazy;
 
 use super::LbPolicyBuilder;
 
@@ -39,4 +37,4 @@ impl Default for LbPolicyRegistry {
 
 /// The registry used if a local registry is not provided to a channel or if it
 /// does not exist in the local registry.
-pub static GLOBAL_LB_REGISTRY: Lazy<LbPolicyRegistry> = Lazy::new(LbPolicyRegistry::new);
+pub static GLOBAL_LB_REGISTRY: LazyLock<LbPolicyRegistry> = LazyLock::new(LbPolicyRegistry::new);
