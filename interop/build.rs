d244567f66c98b4d53d715931597502f0e2594c2
Arjan Singh Bal <46515553+arjan-bal@users.noreply.github.com>
2025-07-30 21:37:28 +0530

    feat(grpc): Add `protobuf` codegen (#2320)

--- interop/build.rs
+++ interop/build.rs
@@ -2,6 +2,11 @@ fn main() {
     let proto = "proto/grpc/testing/test.proto";
 
     tonic_prost_build::compile_protos(proto).unwrap();
+    tonic_protobuf_build::CodeGen::new()
+        .include("proto/grpc/testing")
+        .inputs(["test.proto", "empty.proto", "messages.proto"])
+        .compile()
+        .unwrap();
 
     // prevent needing to rebuild if files (or deps) haven't changed
     println!("cargo:rerun-if-changed={proto}");
