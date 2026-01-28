6611b831ab9e02dd5483c673281fa99efc23f4b6
janlugt <janlugt@gmail.com>
2026-01-28 16:44:07 +0100

    Remove metadata from `tonic::Status` `Display` impl. (#2481)
    Fixes #2480.

--- tonic/src/status.rs
+++ tonic/src/status.rs
@@ -759,9 +759,6 @@ impl fmt::Display for Status {
         if let Some(source) = self.source() {
             write!(f, ", source: {source:?}")?;
         }
-        if let Some(source) = self.source() {
-            write!(f, ", source: {source:?}")?;
-        }
         Ok(())
     }
 }
