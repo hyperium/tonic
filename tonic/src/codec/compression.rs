4e55c45c52b22d531247ba62be47cf8603a8a71d
Apirat Puttaraksa <apirat.puttaraksa@agoda.com>
2026-02-13 17:00:46 +0700

    Revert "println to debug"
    This reverts commit 63f6667015e10f30fca2746c0bae5b28bd632f2b.

--- tonic/src/codec/compression.rs
+++ tonic/src/codec/compression.rs
@@ -247,8 +247,6 @@ pub(crate) fn compress(
     let capacity = ((len / buffer_growth_interval) + 1) * buffer_growth_interval;
     out_buf.reserve(capacity);
 
-    println!("Compressing with encoding: {:?}", settings.encoding);
-
     #[cfg(any(
         feature = "gzip",
         feature = "deflate",
@@ -332,8 +330,6 @@ pub(crate) fn decompress(
         feature = "snappy"
     ))]
     let mut out_writer = out_buf.writer();
-
-    println!("Decompressing with encoding: {:?}", settings.encoding);
 
     match settings.encoding {
         #[cfg(feature = "gzip")]
