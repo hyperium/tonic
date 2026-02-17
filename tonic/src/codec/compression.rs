2f3832138430cd76db9dc2c268c59d8bbfca1ef4
Apirat Puttaraksa <apirat.puttaraksa@agoda.com>
2026-02-17 14:48:42 +0700

    Try to get compression ratio

--- tonic/src/codec/compression.rs
+++ tonic/src/codec/compression.rs
@@ -237,6 +237,8 @@ pub(crate) fn compress(
     let capacity = ((len / buffer_growth_interval) + 1) * buffer_growth_interval;
     out_buf.reserve(capacity);
 
+    let before_compress_len = out_buf.len();
+
     #[cfg(any(
         feature = "gzip",
         feature = "zstd",
@@ -284,6 +286,14 @@ pub(crate) fn compress(
     }
 
     decompressed_buf.advance(len);
+
+    let after_compress_len = out_buf.len();
+
+    println!(
+        "Compressed {len} bytes into {} bytes with {:?}",
+        after_compress_len - before_compress_len,
+        settings.encoding
+    );
 
     Ok(())
 }
