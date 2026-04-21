d7c4e8d22773ce3684764342bd9fbd09da659f7f
Sachin Bhat <sachubhat17@gmail.com>
2026-04-21 12:43:38 +0800

    fix: update rand 0.10 and stdlib API usage
    rand 0.10 moved random_range to the RngExt trait; update all call
    sites. Replace criterion::black_box (deprecated) with std::hint::black_box.
    Replace x % 2 == 0 with x.is_multiple_of(2) per clippy.
    
    Signed-off-by: Sachin Bhat <sachubhat17@gmail.com>

--- grpc/benches/metadata.rs
+++ grpc/benches/metadata.rs
@@ -23,7 +23,7 @@
  */
 
 use criterion::Criterion;
-use criterion::black_box;
+use std::hint::black_box;
 use criterion::criterion_group;
 use criterion::criterion_main;
 use grpc::metadata::MetadataKey;
