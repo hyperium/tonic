d8bba7a05c1bbf8a0191bdaa24a3cb3f5a22abb5
Arjan Bal <arjansbal@google.com>
2026-04-29 01:50:11 +0530

    Revert "ergonomic protobuf build"
    This reverts commit 8a76c02d1b414bfc402531f73cedfdfee77498e4.

--- grpc-protobuf-build/src/lib.rs
+++ grpc-protobuf-build/src/lib.rs
@@ -22,11 +22,9 @@
  *
  */
 
-use std::fs::read_to_string;
-use std::fs::{self};
+use std::fs::{self, read_to_string};
 use std::io::Write;
-use std::path::Path;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use syn::parse_file;
 
@@ -181,7 +179,7 @@ impl CodeGen {
     }
 
     /// Add a directory for protoc to scan for .proto files.
-    pub fn includes(&mut self, includes: impl IntoIterator<Item = impl AsRef<Path>>) -> &mut Self {
+    pub fn includes(&mut self, includes: impl Iterator<Item = impl AsRef<Path>>) -> &mut Self {
         self.includes.extend(
             includes
                 .into_iter()
